2983f3d22a4edb2fee0998ad170422f6bc92676c
Jueqi Liu <ljq1389@gmail.com>
2025-06-08 18:12:21 +0800

    deleted:    .gitattributes 	deleted:    .gitignore 	deleted:    LICENSE 	deleted:    README.md 	deleted:    assets/paper_thumbnail.png 	deleted:    assets/teaser.gif 	deleted:    conf/config.yaml 	deleted:    conf/dataset/abc.yaml 	deleted:    conf/dataset/abc_mini.yaml 	deleted:    conf/dataset/base.yaml 	deleted:    conf/hydra/job_logging/custom.yaml 	deleted:    conf/model/paco.yaml 	deleted:    error.text 	deleted:    inference.py 	deleted:    install.sh 	deleted:    paco/__init__.py 	deleted:    paco/paco_pipeline.py 	deleted:    paco/refinement.py 	deleted:    paco/transformer_utils.py 	deleted:    requirements.txt 	deleted:    scripts/install_polyfit.sh 	deleted:    scripts/test.sh 	deleted:    scripts/train_ddp.sh 	deleted:    scripts/train_dp.sh 	deleted:    test.py 	deleted:    train.py 	deleted:    utils/average_meter.py 	deleted:    utils/builder.py 	deleted:    utils/config.py 	deleted:    utils/data_utils/__init__.py 	deleted:    utils/data_utils/abc_dataset.py 	deleted:    utils/data_utils/build.py 	deleted:    utils/data_utils/data_transforms.py 	deleted:    utils/data_utils/io.py 	deleted:    utils/dist_utils.py 	deleted:    utils/evaluator.py 	deleted:    utils/logger.py 	deleted:    utils/misc.py 	deleted:    utils/registry.py 	deleted:    utils/runner.py 	deleted:    utils/solver.py
     Untracked files:
    	paco_pvd/

--- paco/__init__.py
+++
@@ -1,9 +0,0 @@
-from utils import registry
-MODELS = registry.Registry('models')
-
-from .paco_pipeline import PaCo
-from .refinement import (
-    FixedAdaptivePacoRefinementModule,  
-    ResidualLoss, 
-    SVDProjectionLoss
-)
